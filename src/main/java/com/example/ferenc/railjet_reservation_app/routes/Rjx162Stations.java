6427e8d3ae5f04e1200d0c6f195b75fcdff73ad8
Ferenc Paronai <123371391+ParoniumIsotop137@users.noreply.github.com>
2023-05-16 14:24:52 +0200

    v0.2.4.2

--- src/main/java/com/example/ferenc/railjet_reservation_app/routes/Rjx162Stations.java
+++ src/main/java/com/example/ferenc/railjet_reservation_app/routes/Rjx162Stations.java
@@ -2,7 +2,7 @@ package com.example.ferenc.railjet_reservation_app.routes;
 
 public enum Rjx162Stations {
 
-    BUDAPESTKELENFOLD("Budapest-Kelenföld",1),TATABANYA("Tatabánya",2),GYOR("Győr", 3),MOSONMAGYAROVAR("Mosonmagyaróvár",4), HEGYESHALOM("Hegyeshalom",5),WIENHBF("Wien Hbf",6),WIENMEIDLING("Wien-Meidling",7),STPOLTEN("St. Pölten Hbf",8),LINZ("Linz Hbf",9),SALZBURG("Salzburg Hbf",10),KUFSTEIN("Kustein",11), WORGL("Wörgl",12), INNSBRUCK("Innsbruck Hbf",13),OTZTAL("Ötztal",14), LANDECK("Landeck-Zams",15),STANTON("St. Anton am Arlberg",16),BLUDENZ("Bludenz",17), FELDKIRCH("Feldkirch",18), BUCHS("Buchs SG",19), SARGANS("Sargans",20), ZURICH("Zürich Hbf",21);
+    BUDAPESTKELETI("Budapest-Keleti 6:40",0), BUDAPESTKELENFOLD("Budapest-Kelenföld 6:55",1),TATABANYA("Tatabánya 7:26",2),GYOR("Győr 8:02", 3),MOSONMAGYAROVAR("Mosonmagyaróvár 8:20",4), HEGYESHALOM("Hegyeshalom 8:32",5),WIENHBF("Wien Hbf 9:30",6),WIENMEIDLING("Wien-Meidling 9:37",7),STPOLTEN("St. Pölten Hbf 10:00",8),LINZ("Linz Hbf 10:46",9),SALZBURG("Salzburg Hbf 11:56",10),KUFSTEIN("Kustein 13:09",11), WORGL("Wörgl 13:19",12), INNSBRUCK("Innsbruck Hbf 13:47",13),OTZTAL("Ötztal 14:12",14), LANDECK("Landeck-Zams 14:33",15),STANTON("St. Anton am Arlberg 14:57",16),BLUDENZ("Bludenz 15:31",17), FELDKIRCH("Feldkirch 15:48",18), BUCHS("Buchs SG 16:11",19), SARGANS("Sargans 16:23",20), ZURICH("Zürich Hbf 17:20",21);
 
     private final String name;
     private final int id;
