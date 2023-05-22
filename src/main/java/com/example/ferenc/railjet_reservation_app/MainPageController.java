a9102d3df194e4fe186178b7a04a8d07e3d4a103
Ferenc Paronai <123371391+ParoniumIsotop137@users.noreply.github.com>
2023-05-22 13:55:57 +0200

    v0.3.2

--- src/main/java/com/example/ferenc/railjet_reservation_app/MainPageController.java
+++ src/main/java/com/example/ferenc/railjet_reservation_app/MainPageController.java
@@ -3,6 +3,31 @@ package com.example.ferenc.railjet_reservation_app;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.layout.AnchorPane;
+
 public class MainPageController {
+
+    @FXML
+    private Spinner<Integer> chBoxNumberOfPersons;
+
+    @FXML
+    private ChoiceBox<String> chBoxTrainClass;
+
+    @FXML
+    private ChoiceBox<String> chBoxTrainNumber;
+
+    @FXML
+    private Label lblFreePlaces;
+
+    @FXML
+    private AnchorPane mainAnchorPane;
+
+
+
+
 
 }
